ed91ada0603eabeb1b97b1f70fd51d5471372bfd
Jetix24 <josepabloaguirre02@gmail.com>
2023-09-04 15:31:47 -0600

    Colores y logos

--- app/src/main/java/com/example/intellicube/MainActivity.java
+++ app/src/main/java/com/example/intellicube/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_ayuda, R.id.nav_configuracion)
+                R.id.nav_incubadora, R.id.nav_informacion, R.id.nav_perfil, R.id.nav_ayuda, R.id.nav_configuracion)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
